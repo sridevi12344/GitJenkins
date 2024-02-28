5e0ab6f6ac1f13253698e9bac379963b4d44fb37
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-02-28 10:39:17 +0000

    Update gitJen.java
    Added one statement

--- gitJen.java
+++ gitJen.java
@@ -3,5 +3,6 @@ public class Myjava
 public static void main (String args[])
 {
 System.out.println("Jenkins and Git are DevOps tools");
+System.out.println("Git is used as a plugin to jenkins");
 }
 }
