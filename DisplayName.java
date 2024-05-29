b71463f0da474234786a19e36ac0e8267b8c645f
sridevi12344 <60096252+sridevi12344@users.noreply.github.com>
2024-05-29 13:48:00 +0000

    Update DisplayName.java

--- DisplayName.java
+++ DisplayName.java
@@ -3,7 +3,7 @@ public class DisplayName
 public static void main(String []args )
 {
 System.out.println("Success");
-System.out.println("Wipro");
+System.out.println("Attending Git-Jenkins integration class");
 
 }
 }
