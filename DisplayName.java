f06bea75ffedcd5c624fe351c6076f21470faa70
sridevi12344 <60096252+sridevi12344@users.noreply.github.com>
2024-05-29 10:45:39 +0000

    Update DisplayName.java

--- DisplayName.java
+++ DisplayName.java
@@ -2,7 +2,7 @@ public class DisplayName
 {
 public static void main(String []args )
 {
-System.out.println("My name is printed");
+System.out.println("My name is printed correctly");
 System.out.println("We are Learning Jenkins with GitHub repositories");
 
 }
