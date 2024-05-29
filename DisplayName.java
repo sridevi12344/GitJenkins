5cf9c14465a2f9595c680284fa6c469581be0e29
sridevi12344 <60096252+sridevi12344@users.noreply.github.com>
2024-05-29 13:42:36 +0000

    Update DisplayName.java

--- DisplayName.java
+++ DisplayName.java
@@ -3,7 +3,7 @@ public class DisplayName
 public static void main(String []args )
 {
 System.out.println("Success");
-System.out.println("We are Learning Jenkins with GitHub repositories");
+System.out.println("Wipro");
 
 }
 }
