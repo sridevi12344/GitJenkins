28ca1cf1ba48427201cec5a97ad126ed999f4beb
sridevi12344 <60096252+sridevi12344@users.noreply.github.com>
2024-05-29 13:21:24 +0000

    Delete gitJen.java

--- gitJen.java
+++
@@ -1,8 +0,0 @@
-public class gitJen
-{
-public static void main (String args[])
-{
-System.out.println("Jenkins and Git are DevOps tools");
-System.out.println("Git is used as a plugin to jenkins");
-}
-}
